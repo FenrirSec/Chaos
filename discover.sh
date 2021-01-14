35ba4ca5152c19e0a528bd9c4e39657caae169ce
Lp1 <mail@lp1.eu>
2021-01-14 20:16:29 +0100

    Update discover.sh

--- discover.sh
+++ discover.sh
@@ -3,6 +3,6 @@
 echo "Searching for a connected Raspberry Pi..."
 IP=`ip --brief address show | grep wlan1 | grep -Eo 192.168.[0-9]+\.[0-9]+\/[0-9]+`
 PHONE_IP=`echo $IP | grep -Eo 192.168.[0-9]+\.[0-9]+`
-echo "Your phone address is $PHONE_IP"
+# echo "Your phone address is $PHONE_IP"
 RPI_IP=`nmap -sn -PE -T5 $IP 2> /dev/null | grep 'report for' | grep -v "$PHONE_IP" | grep -Eo 192.168.[0-9]+\.[0-9]+`
 echo "RPI is at $RPI_IP"
