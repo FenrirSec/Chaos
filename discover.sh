7b7c61bcb555fe8503ac462723ecb73ba8a090fb
Lp1 <mail@lp1.eu>
2021-01-14 20:10:09 +0100

    Updated scripts to avoir duplication and added autoconf scripts

--- discover.sh
+++ discover.sh
@@ -1,9 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/env bash
 
 echo "Searching for a connected Raspberry Pi..."
 IP=`ip --brief address show | grep wlan1 | grep -Eo 192.168.[0-9]+\.[0-9]+\/[0-9]+`
 PHONE_IP=`echo $IP | grep -Eo 192.168.[0-9]+\.[0-9]+`
 RPI_IP=`nmap -sn -PE -T5 $IP 2> /dev/null | grep 'report for' | grep -v $PHONE_IP | grep -Eo 192.168.[0-9]+\.[0-9]+`
 echo "RPI is at $RPI_IP"
-echo "Connecting to the RPI"
-ssh "lp1@$RPI_IP"
