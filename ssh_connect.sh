f9b1a9514f7f7ff9535a856159d8285e15a5da35
Lp1 <mail@lp1.eu>
2021-01-14 20:15:38 +0100

    fixed syntax error in scripts

--- ssh_connect.sh
+++ ssh_connect.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-$RPI_IP = `bash discover.sh | grep -Eo 192.168.[0-9]+\.[0-9]+`
+RPI_IP=`bash discover.sh | grep -Eo 192.168.[0-9]+\.[0-9]+`
 echo "Connecting to the RPI at $RPI_IP..."
 ssh "lp1@$RPI_IP"
--- ssh_setup.sh
+++ ssh_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-$RPI_IP = `bash discover.sh | grep -Eo 192.168.[0-9]+\.[0-9]+`
+RPI_IP=`bash discover.sh | grep -Eo 192.168.[0-9]+\.[0-9]+`
 echo "Sending files to the RPI at $RPI_IP"
 scp ~/.ssh/id_rsa.pub "lp1@$RPI_IP:~/pubkey"
 ssh lp1@$RPI_IP 'cat pubkey >> .ssh/authorized_keys'
