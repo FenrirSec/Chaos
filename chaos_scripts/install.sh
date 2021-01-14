29413b860cfed310f8149897f99fb2f5ca0ebf7e
Lp1 <mail@lp1.eu>
2021-01-14 20:55:49 +0100

    Update install.sh

--- chaos_scripts/install.sh
+++ chaos_scripts/install.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
+
+CHAOS_USER="lp1"
 echo "[ ~ Chaos setup ~ ]"
 if [ "$EUID" -ne 0 ];then 
   echo "Please run as root"
   exit
 fi
 echo "chaos" >> /etc/hostname
-echo "lp1 ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+if grep -q "$CHAOS_USER ALL=(ALL) NOPASSWD: ALL" /etc/sudoers
+then 
+   echo "Sudoers file has already been setup for $CHAOS_USER";
+else
+  echo "$CHAOS_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+  echo "Added $CHAOS_USER to sudoers"
+fi
