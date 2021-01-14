e41af66620cfce6be9c50d2008324a2b2bfa3c4f
Lp1 <mail@lp1.eu>
2021-01-14 20:02:13 +0100

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -2,5 +2,6 @@
 
 echo "Searching for a connected Raspberry Pi"
 IP=`ip --brief address show | grep wlan1 | grep -Eo 192.168.[0-9]+\.[0-9]+\/[0-9]+`
-RPI=`nmap -sn -PE -T5 $IP | grep 'report for' | grep -Eo 192.168.[0-9]+\.[0-9]+`
-echo "RPI is at $RPI"
+PHONE_IP=`echo $IP | grep -Eo 192.168.[0-9]+\.[0-9]+`
+RPI_IP=`nmap -sn -PE -T5 $IP | grep 'report for' | grep -v $PHONE_IP | grep -Eo 192.168.[0-9]+\.[0-9]+`
+echo "RPI is at $RPI_IP"
