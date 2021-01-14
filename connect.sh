df5ead3fcf70e2d08615e8162051ec15eed32176
Lp1 <mail@lp1.eu>
2021-01-14 19:58:23 +0100

    Update connect.sh

--- connect.sh
+++ connect.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Searching for a connected Raspberry Pi"
-IP=`ip addr wlan1 | grep -Eo 192.168.[0-9]+`
-nmap -sn -PE -T5 "$IP.0/24"
+IP=`ip --brief address show | grep wlan1 | grep -Eo 192.168.[0-9]+\.[0-9]+\/[0-9]+`
+nmap -sn -PE -T5 $IP
