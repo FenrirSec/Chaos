ef9c2d223ddabba7152abc52ac4458e059eba07b
Jéremie Amsellem <mail@lp1.eu>
2021-01-21 18:29:26 +0100

    updated ssh_setup

--- termux_scripts/ssh_setup.sh
+++ termux_scripts/ssh_setup.sh
@@ -1,6 +1,8 @@
 #!/bin/env bash
 
+echo "Username?"
+read USERNAME
 RPI_IP=`bash discover.sh | grep -Eo 192.168.[0-9]+.[0-9]+`
 echo "Sending files to the RPI at $RPI_IP"
-scp ~/.ssh/id_rsa.pub "lp1@$RPI_IP:~/pubkey"
-ssh lp1@$RPI_IP 'cat pubkey >> .ssh/authorized_keys;rm pubkey'
+scp ~/.ssh/id_rsa.pub "$USERNAME@$RPI_IP:~/pubkey"
+ssh $USERNAME@$RPI_IP 'mkdir .ssh && cat pubkey >> .ssh/authorized_keys;rm pubkey'
